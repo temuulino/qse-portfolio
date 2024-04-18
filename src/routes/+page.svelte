eb585c9a98e1844d434fc77e96b4450af9ff1279
temuulino <temka348@gmail.com>
2024-04-18 12:16:56 +0800

    card component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
-<!-- import.meta.env.VITE_GRAPHQL_API -->
 <script>
+  import ProjectCard from '../lib/components/project-card.svelte';
   import { onMount } from 'svelte';
   import { request, gql } from 'graphql-request';
 
-  let data = null;
+  let projects = null;
   const endpoint = import.meta.env.VITE_GRAPHQL_API;
 
   const query = gql`
@@ -23,25 +23,31 @@
 
   onMount(async () => {
     try {
-      data = await request(endpoint, query);
+      const { projects: fetchedProjects } = await request(endpoint, query);
+      projects = fetchedProjects;
     } catch (error) {
       console.error(error);
     }
   });
+
 </script>
 
+<h1>
+  Recent Projects By QSE
+</h1>
+
 <main>
-  {#if data}
-    <pre>{JSON.stringify(data, null, 2)}</pre>
+  {#if projects}
+    {#each projects as { name, slug, description, image }}
+      <div>
+        <ProjectCard {name} {description} url={image[0].url} {slug} />
+      </div>
+    {/each}
   {:else}
     <p>Loading...</p>
   {/if}
 </main>
 
 <style>
-  pre {
-    background-color: #f4f4f4;
-    padding: 1rem;
-    border-radius: 8px;
-  }
+  /* Add your CSS styles here */
 </style>
